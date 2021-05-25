77c98b73693f9acc4ff2864e26fd374282901a7c
Erick Díaz <erickdp@hotmail.com>
2021-05-25 16:26:21 -0500

    BindingResult
    Se usa este componente para la validacion de los campos

--- src/main/java/com/example/demo/controllers/FormController.java
+++ src/main/java/com/example/demo/controllers/FormController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controllers;
 
 import com.example.demo.domain.Usuario;
+import java.util.HashMap;
+import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -21,9 +24,20 @@ public class FormController {
     
     // Se extraen los valores a traves del protocolo http request
     @PostMapping("/form")
-    public String procesar(Usuario usuario, Model model) { 
-//        Se mappea de manera automatica al tener el mismo nombre de los campos en el html, es necesario tener setter en la entidad
+    public String procesar(@Valid Usuario usuario, BindingResult result, Model model) { // En caso de errores en la validacion se usa binding, debe de estar siempre despues del objeto que se valida
+        
         model.addAttribute("titulo", "Resultado Form");
+        
+        if(result.hasErrors()) {
+            var errores = new HashMap<String, String>();
+            result.getFieldErrors().forEach(err -> {
+                errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage())); // Se obtiene el input donde fallo y el valor
+            });
+            model.addAttribute("error", errores);
+            return "form";
+        }
+        
+//        Se mappea de manera automatica al tener el mismo nombre de los campos en el html, es necesario tener setter en la entidad
         model.addAttribute("usuario", usuario);
         
         return "resultado";
--- src/main/java/com/example/demo/domain/Usuario.java
+++ src/main/java/com/example/demo/domain/Usuario.java
@@ -1,13 +1,20 @@
 package com.example.demo.domain;
 
+import javax.validation.constraints.NotEmpty;
+
 /**
  *
  * @author Erick Diaz
  */
 public class Usuario {
 
+    @NotEmpty // Sea distinto de null y que tenga longitud mayor a 0
     private String username;
+    
+    @NotEmpty
     private String password;
+    
+    @NotEmpty
     private String email;
 
     public String getUsername() {

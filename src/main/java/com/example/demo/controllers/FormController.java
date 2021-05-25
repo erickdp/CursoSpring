3cb79083a72fa8416b6419391ec4accbf4a47e4b
Erick Díaz <erickdp@hotmail.com>
2021-05-25 16:13:45 -0500

    Form agregado
    Archivo form agregado

--- src/main/java/com/example/demo/controllers/FormController.java
+++ src/main/java/com/example/demo/controllers/FormController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -14,11 +15,22 @@ public class FormController {
     
     @GetMapping("/form")
     public String form(Model model) {
+        model.addAttribute("titulo", "Formulario de Usuario");
         return "form";
     }
     
+    // Se extraen los valores a traves del protocolo http request
     @PostMapping("/form")
-    public String procesar(Model model) {
+    public String procesar(Model model, 
+            @RequestParam(name = "username") String username, // Se puede usar para definir que valor se usa para extraer
+            @RequestParam String password,
+            @RequestParam String email) {
+        
+        model.addAttribute("titulo", "Resultado Form");
+        model.addAttribute("username", username);
+        model.addAttribute("password", password);
+        model.addAttribute("email", email);
+        
         return "resultado";
     }
     

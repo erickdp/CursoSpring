76144e671ecbfb68a25e473ae787281e0f1a7c70
Erick Díaz <erickdp@hotmail.com>
2021-05-25 16:47:04 -0500

    Mensajes de error
    Se hace uso además de modelAttribute

--- src/main/java/com/example/demo/controllers/FormController.java
+++ src/main/java/com/example/demo/controllers/FormController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 /**
@@ -17,15 +18,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class FormController {
     
     @GetMapping("/form")
-    public String form(Model model) {
+    public String form(@ModelAttribute("user") Usuario usuario, Model model) { // El objeto se inicializa y pasa al formulario de manera automatica o se puede hacer manual con model.add...
         model.addAttribute("titulo", "Formulario de Usuario");
         return "form";
     }
     
     // Se extraen los valores a traves del protocolo http request
     @PostMapping("/form")
-    public String procesar(@Valid Usuario usuario, BindingResult result, Model model) { // En caso de errores en la validacion se usa binding, debe de estar siempre despues del objeto que se valida
-        
+    public String procesar(@Valid @ModelAttribute("user") Usuario usuario, BindingResult result, Model model) { // En caso de errores en la validacion se usa binding, debe de estar siempre despues del objeto que se valida
+        // ModdelAttribute se usa para definir con que nombre se va a pasar el objeto a la vista
         model.addAttribute("titulo", "Resultado Form");
         
         if(result.hasErrors()) {
@@ -38,7 +39,7 @@ public class FormController {
         }
         
 //        Se mappea de manera automatica al tener el mismo nombre de los campos en el html, es necesario tener setter en la entidad
-        model.addAttribute("usuario", usuario);
+//        model.addAttribute("usuario", usuario); // El Pojo usuario se pasa al formulario de manera automatica o manual
         
         return "resultado";
     }

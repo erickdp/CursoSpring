c8ba1de34f9735366c14c5cbb3c773a1003d3c32
Erick Díaz <erickdp@hotmail.com>
2021-05-25 17:12:22 -0500

    Atributo SessionAttribute
    Este atributo se usa para guardar atributos de sesion y que se mantengan cuando no quiero mostrarlos

--- src/main/java/com/example/demo/controllers/FormController.java
+++ src/main/java/com/example/demo/controllers/FormController.java
@@ -8,12 +8,15 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 /**
  *
  * @author Erick Diaz
  */
 @Controller
+@SessionAttributes("user") // Este att se guarda en una session http, pero si se cambian se actualizan
 public class FormController {
     
     @GetMapping("/form")
@@ -30,7 +33,7 @@ public class FormController {
     
     // Se extraen los valores a traves del protocolo http request
     @PostMapping("/form")
-    public String procesar(@Valid @ModelAttribute("user") Usuario usuario, BindingResult result, Model model) { // En caso de errores en la validacion se usa binding, debe de estar siempre despues del objeto que se valida
+    public String procesar(@Valid @ModelAttribute("user") Usuario usuario, BindingResult result, Model model, SessionStatus status) { // En caso de errores en la validacion se usa binding, debe de estar siempre despues del objeto que se valida
         // ModdelAttribute se usa para definir con que nombre se va a pasar el objeto a la vista
         model.addAttribute("titulo", "Resultado Form");
         
@@ -40,7 +43,7 @@ public class FormController {
         
 //        Se mappea de manera automatica al tener el mismo nombre de los campos en el html, es necesario tener setter en la entidad
 //        model.addAttribute("usuario", usuario); // El Pojo usuario se pasa al formulario de manera automatica o manual
-        
+        status.setComplete(); // Limpia los datos usados en sessionAttributes
         return "resultado";
     }
     

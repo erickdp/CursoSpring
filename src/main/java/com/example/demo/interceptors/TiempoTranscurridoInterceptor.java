15e0522f383606f58efab8fc3028ccb3bfd44834
Erick Díaz <erickdp@hotmail.com>
2021-06-01 18:25:06 -0500

    Interceptores
    La materia esta en Inyeccion de Dependencias en descargar de la pc

---
+++ src/main/java/com/example/demo/interceptors/TiempoTranscurridoInterceptor.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.interceptors;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ *
+ * @author Erick Diaz
+ */
+public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
+    
+}

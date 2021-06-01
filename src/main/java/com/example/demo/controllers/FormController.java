95cbe9fce7a4a5cb08984adc04436f4f152449d8
Erick Díaz <erickdp@hotmail.com>
2021-06-01 17:15:30 -0500

    Ajustando formulario
    Se usaron propiedades para poder evitar procesamiento de datos duplicados

--- src/main/java/com/example/demo/controllers/FormController.java
+++ src/main/java/com/example/demo/controllers/FormController.java
@@ -27,6 +27,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
@@ -36,29 +37,29 @@ import org.springframework.web.bind.support.SessionStatus;
  */
 @Controller
 @SessionAttributes("user") // Este att se guarda en una session http, pero si se cambian se actualizan
-public class FormController {    
-    
+public class FormController {
+
     @Autowired
     private UsuarioValidador validador;
-    
+
     @Autowired
     private PaisService paisService;
-    
+
     @Autowired
     private PaisPropertyEditor paisPropertyEditor;
-    
+
     @Autowired
     private RoleService roleService;
-    
+
     @Autowired
     private RolePropertyEditor rolePropertyEditor;
 
     // El validador de las anotaciones por defecto se cambia por el personalizado
-    @InitBinder    
+    @InitBinder
     public void initBinder(WebDataBinder binder) { // Otra forma de validar los parametros enviados es usando este metodo
 //        binder.setValidator(validador); Valida solo los datos definidos en la calse validador
         binder.addValidators(validador); // Valida todos
-        
+
         var sdf = new SimpleDateFormat("yyyy-MM-dd"); // Solo este formato es valido para las fechas en html
         sdf.setLenient(false); // Permite que no haya fechas sin formatos, no es tolerante
         binder.registerCustomEditor(Date.class, new CustomDateEditor(sdf, false));
@@ -66,13 +67,13 @@ public class FormController {
 //        Registramos para que se pueda usar mayusculas
         binder.registerCustomEditor(String.class, "nombre", new NombreMayusculaEditor()); // Pasamos nuestro custom editor y el campo específico a pasarlo a mayus, si no se define lo toma para todos los campos
         binder.registerCustomEditor(String.class, "apellido", new NombreMayusculaEditor());
-        
+
 //        Se registra para poder validar el objeto de pais pero debo de enviar solamente el valor de id
         binder.registerCustomEditor(Pais.class, "pais", this.paisPropertyEditor);
-        
+
         binder.registerCustomEditor(Role.class, "roles", this.rolePropertyEditor);
     }
-    
+
     @GetMapping("/form")
     public String form(Model model) { // El objeto se inicializa y pasa al formulario de manera automatica o se puede hacer manual con model.add...
         var usuario = new Usuario();
@@ -83,7 +84,7 @@ public class FormController {
         usuario.setPais(new Pais(3, "EC", "Ecuador"));
         usuario.setRoles(Arrays.asList(new Role(1, "Administrador", "ROL_ADMIN")));
         usuario.setValorSecreto("Algo secreto"); // Este campo se va a usar con el atributo hidden en las etiquetas de los archivos html sirve como @sessionAttribute
-         
+
         model.addAttribute("user", usuario);
         model.addAttribute("titulo", "Formulario de Usuario");
         return "form";
@@ -91,25 +92,36 @@ public class FormController {
 
     // Se extraen los valores a traves del protocolo http request
     @PostMapping("/form")
-    public String procesar(@Valid @ModelAttribute("user") Usuario usuario, BindingResult result, Model model, SessionStatus status) { // En caso de errores en la validacion se usa binding, debe de estar siempre despues del objeto que se valida
+    public String procesar(@Valid @ModelAttribute(name = "user") Usuario usuario, BindingResult result, Model model) { // En caso de errores en la validacion se usa binding, debe de estar siempre despues del objeto que se valida
         // ModdelAttribute se usa para definir con que nombre se va a pasar el objeto a la vista
-        model.addAttribute("titulo", "Resultado Form");
-        
+
         if (result.hasErrors()) {
+            model.addAttribute("titulo", "Resultado Form");
             return "form";
         }
 
 //        Se mappea de manera automatica al tener el mismo nombre de los campos en el html, es necesario tener setter en la entidad
 //        model.addAttribute("usuario", usuario); // El Pojo usuario se pasa al formulario de manera automatica o manual
+        return "redirect:/ver"; // Al hacer una nueva peticion los datos ya no quedan guardados
+    }
+
+//    Este metodo es para que cuando se cargue neuvamente el formulario los datos no se queden guardando y evitando duplicados 
+    @GetMapping("/ver")
+    public String ver(@SessionAttribute(name = "user", required = false) Usuario usuario, Model model, SessionStatus status) {
+//        El usuario se pasa a la vista de manera automatica como ya se a visto
+        if (usuario == null) {
+            return "redirect:/form";
+        }
+        model.addAttribute("titulo", "Resultado Form");
         status.setComplete(); // Limpia los datos usados en sessionAttributes
         return "resultado";
     }
-    
+
     @ModelAttribute("paises") // Este metodo sera tomado por todo los metodos handler para ser usado en la vista, paises sera el nombre para acceder
     public List<String> paises() {
         return Arrays.asList("Mexico", "Chile", "Ecuador", "Colombia", "Venezuela");
     }
-    
+
     @ModelAttribute("paisesMap")
     public Map<String, String> paisesMap() {
         var paises = new HashMap<String, String>();
@@ -120,12 +132,12 @@ public class FormController {
         paises.put("MX", "Mexico");
         return paises;
     }
-    
+
     @ModelAttribute("listaPaises")
     public List<Pais> listaPaises() {
         return this.paisService.listar();
     }
-    
+
     @ModelAttribute("listaRolesString")
     public List<String> listaRolesString() {
         var roles = new ArrayList<String>();
@@ -134,7 +146,7 @@ public class FormController {
         roles.add("ROL_MODERATOR");
         return roles;
     }
-    
+
     @ModelAttribute("listaRolesMap")
     public Map<String, String> listaRolesMap() {
         var roles = new HashMap<String, String>();
@@ -143,15 +155,15 @@ public class FormController {
         roles.put("ROL_MODERATOR", "Moderador");
         return roles;
     }
-    
+
     @ModelAttribute("listaRoles")
     public List<Role> listaRoles() {
         return this.roleService.listar();
     }
-    
+
     @ModelAttribute("generos")
     private List<String> listaGeneros() {
         return Arrays.asList("Hombre", "Mujer");
     }
-    
+
 }
